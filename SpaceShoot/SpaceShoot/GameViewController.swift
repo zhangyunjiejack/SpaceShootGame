0fc301d7fc19d4541238208eea3744c613e4b42b
zhangyunjiejack <zhangyunjiejack@gmail.com>
2017-12-05 20:53:51 -0800

    almost finished. Game over and about us to be finished

---
+++ SpaceShoot/SpaceShoot/GameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GameViewController.swift
+//  SpaceShoot
+//
+//  Created by 张云杰 on 2017/11/29.
+//  Copyright © 2017年 TeamNoobs. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            if let scene = SKScene(fileNamed: "Menu") {
+                scene.scaleMode = .aspectFill
+                
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ SpaceShoot/SpaceShoot/menuScene.swift
@@ -0,0 +1,38 @@
+//
+//  menuScene.swift
+//  SpacegameReloaded
+//
+//  Created by 石济 on 11/11/17.
+//  Copyright © 2017 Training. All rights reserved.
+//
+
+import SpriteKit
+
+class menuScene: SKScene {
+    var startBackgroundNode: SKEmitterNode!
+    var newGameButtonNode: SKSpriteNode!
+    var scoresButtonNode: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        startBackgroundNode = self.childNode(withName: "starBackground") as! SKEmitterNode
+        startBackgroundNode.advanceSimulationTime(10)
+        
+        newGameButtonNode = self.childNode(withName: "startButton") as! SKSpriteNode
+        scoresButtonNode = self.childNode(withName: "scoresButton") as! SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        
+        if let location = touch?.location(in: self) {
+            let nodesArray = self.nodes(at: location)
+            
+            if nodesArray.first?.name == "startButton" {
+                let transition = SKTransition.flipVertical(withDuration: 0.5)
+                let gameScene = GameScene(size: self.size)
+                self.view?.presentScene(gameScene, transition: transition)
+            }
+        }
+    }
+
+}
